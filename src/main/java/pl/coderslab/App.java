9750a0f5c4f81f2c9db95570ac5acdd2c8039ed7
bruno <bruno.kedzierski@wp.pl>
2018-08-07 11:34:09 +0200

    pierw

---
+++ src/main/java/pl/coderslab/App.java
@@ -0,0 +1,35 @@
+package pl.coderslab;
+
+import org.apache.log4j.Logger;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+
+        Connection connect = Jsoup.connect("http://www.onet.pl/");
+        try {
+            Document document = connect.get();
+            Elements links = document.select("span.title");
+            for (Element elem : links) {
+                System.out.println(elem.text());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
+
+
